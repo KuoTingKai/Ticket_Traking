69ee608a0c5f160b3f97c4edbe128fdd9a1eccae
KuoTingKai <cov85741@gmail.com>
2023-03-13 15:11:07 +0800

    modify the staff.

--- src/main/java/com/example/demo/controller/StaffController.java
+++ src/main/java/com/example/demo/controller/StaffController.java
@@ -5,6 +5,7 @@ import com.example.demo.repository.StaffRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/staff")
@@ -14,7 +15,11 @@ public class StaffController {
     public StaffController(StaffRepository staffRepository) {
         this.staffRepository = staffRepository;
     }
-
+    @GetMapping(value = "/{id}")
+    public Optional<Staff> getStaff(@PathVariable("id") String id)
+    {
+        return staffRepository.findById(id);
+    }
 
     @GetMapping
     public List<Staff> getStaff()
@@ -23,13 +28,9 @@ public class StaffController {
     }
 
     @PostMapping
-//    public void addStaff(@RequestBody String json, HttpServletRequest)
     public void addStaff(@RequestBody Staff newstaff)
     {
-        Staff staff = new Staff();
-        staff.setId(newstaff.getId());
-        staff.setType(newstaff.getType());
-        staffRepository.save(staff);
+        staffRepository.save(new Staff(newstaff.getId(), newstaff.getType()));
     }
 
 }
--- src/main/java/com/example/demo/repository/StaffRepository.java
+++ src/main/java/com/example/demo/repository/StaffRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.Staff;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface StaffRepository extends MongoRepository<Staff, String> {
+    List<Staff> findByType(Staff.Type type);
 }
